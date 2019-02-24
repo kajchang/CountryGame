02a1ff358f528a5fce0abe09fee9486e98fad343
Kai Chang <kaijchang@gmail.com>
2019-02-24 11:36:30 -0800

    feat: add readme and license

---
+++ README.md
@@ -0,0 +1,5 @@
+#Country Game
+
+[![Build Status](https://travis-ci.org/kajchang/CountryGame.svg?branch=master)](https://travis-ci.org/kajchang/CountryGame)
+
+A country identification game with a Kotlin frontend.
--- webpack.config.d/html.js
+++ webpack.config.d/html.js
@@ -1,8 +1,11 @@
 const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
-console.log(__dirname);
-
-config.plugins.push(new CopyPlugin([
-    { from: path.join(__dirname, '..', 'src', 'main', 'web', 'index.prod.html'), to: path.join(__dirname, 'index.html')}
-]));
+if (process.env.TRAVIS) {
+    config.plugins.push(new CopyPlugin([
+        {
+            from: path.join(__dirname, '..', 'src', 'main', 'web', 'index.prod.html'),
+            to: path.join(__dirname, 'index.html')
+        }
+    ]));
+}
