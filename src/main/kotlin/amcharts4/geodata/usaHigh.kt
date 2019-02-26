84b1bf52e43258b48ddc0a89595eb9f9f024a54d
Kai Chang <kaijchang@gmail.com>
2019-02-25 16:56:22 -0800

    fix: game resetting

---
+++ src/main/kotlin/amcharts4/geodata/usaHigh.kt
@@ -0,0 +1,6 @@
+@file:JsModule("@amcharts/amcharts4-geodata/worldHigh")
+
+package amcharts4.geodata
+
+@JsName("default")
+external val am4geodata_usHigh: dynamic
--- src/main/kotlin/countrygame/application/CountryPresenter.kt
+++ src/main/kotlin/countrygame/application/CountryPresenter.kt
@@ -7,6 +7,7 @@ import kotlin.browser.window
 class CountryPresenter(override val view: CountryView) : Presenter<CountryView, Map<String, Any?>> {
     private lateinit var selectedRegion: String
     private var gameStarted = false
+    private var firstStart = true
 
     private var countryToFind: String = ""
     private var countries = mutableListOf<String>()
@@ -23,6 +24,10 @@ class CountryPresenter(override val view: CountryView) : Presenter<CountryView, 
     fun startGame() {
         if (!gameStarted) {
             gameStarted = true
+            if (!firstStart) {
+                setRegion(selectedRegion)
+            }
+            firstStart = false
             nextCountry()
             view.updateTimer(timer)
 
@@ -40,7 +45,6 @@ class CountryPresenter(override val view: CountryView) : Presenter<CountryView, 
                 window.clearInterval(interval as Int)
                 interval = null
                 gameStarted = false
-                addCountries(selectedRegion)
                 view.displayWin()
             } else {
                 nextCountry()
@@ -55,68 +59,80 @@ class CountryPresenter(override val view: CountryView) : Presenter<CountryView, 
         selectedRegion = region
 
         val circles = js("[]")
-
-        @Suppress("UNCHECKED_CAST")
-        val include = Regions[region]?.get("include") as MutableList<String>?
-
-        @Suppress("UNCHECKED_CAST")
-        val initialGeoPoint = Regions[region]?.get("initialPoint") as Map<String, Double>
-        val initialZoom = Regions[region]?.get("initialZoom") as Double
+        val initialZoom: Double
+        val initialGeoPoint: Map<String, Double>
 
         reset()
-        addCountries(region)
-        am4geodata_worldHigh.features.forEach { country ->
-            if (include?.contains(country.properties.id as String) != false) {
-                // calculate area and center for small countries
 
-                var biggestArea: Double? = null
+        if (Regions[region] != null) {
+            @Suppress("UNCHECKED_CAST")
+            val include = Regions[region]?.get("include") as MutableList<String>?
 
-                val overallLatCoords = mutableListOf<Double>()
-                val overallLongCoords = mutableListOf<Double>()
+            @Suppress("UNCHECKED_CAST")
+            initialGeoPoint = Regions[region]?.get("initialPoint") as Map<String, Double>
+            initialZoom = Regions[region]?.get("initialZoom") as Double
 
-                for (idx in 0 until country.geometry.coordinates.length as Int) {
-                    val polygon = if (country.geometry.type == "Polygon") country.geometry.coordinates[idx] else country.geometry.coordinates[idx][0]
+            am4geodata_worldHigh.features.forEach { country ->
+                if (include?.contains(country.properties.id as String) != false) {
+                    addCountry(country.properties.name)
 
-                    val latCoords = mutableListOf<Double>()
-                    val longCoords = mutableListOf<Double>()
+                    val countryStats = countryGeo(country)
 
-                    polygon.forEach { point ->
-                        latCoords.add(point[0] as Double)
-                        longCoords.add(point[1] as Double)
+                    val biggestArea = countryStats[0] as Int
+                    @Suppress("UNCHECKED_CAST")
+                    val overallLatCoords = countryStats[1] as List<Double>
+                    @Suppress("UNCHECKED_CAST")
+                    val overallLongCoords = countryStats[2] as List<Double>
+
+                    if (biggestArea < 1) {
+                        val center = centroid(overallLatCoords, overallLongCoords)
+                        circles.push(nativeObject(mapOf(
+                                "latitude" to center[1],
+                                "longitude" to center[0],
+                                "name" to country.properties.name
+                        )))
                     }
-
-                    val area = irregularArea(latCoords, longCoords)
-
-                    if (area > biggestArea ?: 0.0) {
-                        biggestArea = area
-                    }
-
-                    overallLatCoords.addAll(latCoords)
-                    overallLongCoords.addAll(longCoords)
-                }
-
-                if (biggestArea.unsafeCast<Double>() < 1) {
-                    val center = centroid(overallLatCoords, overallLongCoords)
-                    circles.push(nativeObject(mapOf(
-                            "latitude" to center[1],
-                            "longitude" to center[0],
-                            "name" to country.properties.name
-                    )))
                 }
             }
-        }
 
-        view.displayRegion(region, include, initialZoom, initialGeoPoint, circles)
+            view.displayRegion(region, include, initialZoom, initialGeoPoint, circles)
+        }
     }
 
-    private fun addCountries(region: String) {
-        @Suppress("UNCHECKED_CAST")
-        val include = Regions[region]?.get("include") as MutableList<String>?
-        am4geodata_worldHigh.features.forEach { country ->
-            if (include?.contains(country.properties.id as String) != false) {
-                addCountry(country.properties.name as String)
+    private fun countryGeo(country: dynamic): List<Any?> {
+        // calculate area and center for small countries
+
+        var biggestArea: Double? = null
+
+        val overallLatCoords = mutableListOf<Double>()
+        val overallLongCoords = mutableListOf<Double>()
+
+        for (idx in 0 until country.geometry.coordinates.length as Int) {
+            val polygon = if (country.geometry.type == "Polygon") country.geometry.coordinates[idx] else country.geometry.coordinates[idx][0]
+
+            val latCoords = mutableListOf<Double>()
+            val longCoords = mutableListOf<Double>()
+
+            polygon.forEach { point ->
+                latCoords.add(point[0] as Double)
+                longCoords.add(point[1] as Double)
             }
+
+            val area = irregularArea(latCoords, longCoords)
+
+            if (area > biggestArea ?: 0.0) {
+                biggestArea = area
+            }
+
+            overallLatCoords.addAll(latCoords)
+            overallLongCoords.addAll(longCoords)
         }
+
+        return listOf(
+                biggestArea,
+                overallLatCoords,
+                overallLongCoords
+        )
     }
 
     private fun nextCountry() {
