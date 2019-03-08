9809a4c0ce5a65d0de00fbfa65687efaebdaa0dc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-08 00:07:40 +0000

    Deploy kajchang/CountryGame to github.com/kajchang/CountryGame.git:gh-pages

---
+++ xlsx.bundle.js
@@ -0,0 +1,13 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["xlsx"],[
+/*!********************!*\
+  !*** fs (ignored) ***!
+  \********************/
+/*! no static exports found */function(module,exports){eval("/* (ignored) */\n\n//# sourceURL=webpack:///fs_(ignored)?")},
+/*!************************!*\
+  !*** crypto (ignored) ***!
+  \************************/
+/*! no static exports found */function(module,exports){eval("/* (ignored) */\n\n//# sourceURL=webpack:///crypto_(ignored)?")},
+/*!************************!*\
+  !*** stream (ignored) ***!
+  \************************/
+/*! no static exports found */function(module,exports){eval("/* (ignored) */\n\n//# sourceURL=webpack:///stream_(ignored)?")}]]);
